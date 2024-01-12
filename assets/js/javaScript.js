d5d1c998a709b9cfdb4f42162fa28769fd4728d5
Hroyero <hroyero22@gmail.com>
2024-01-12 10:46:27 -0500

    A js functionality the sameThe same functionality was created in js but for my jobs

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -4,4 +4,8 @@
             console.log("SE CLIKEO AQUI");
             document.getElementById('Acerca-de-mi').scrollIntoView({ behavior: 'smooth' });
         });
+        document.getElementById('seccionB').addEventListener('click', function () {
+            console.log("SE CLIKEO AQUI en mis trabajos");
+            document.getElementById('Mis-trabajos').scrollIntoView({ behavior: 'smooth' });
+        });
     });
